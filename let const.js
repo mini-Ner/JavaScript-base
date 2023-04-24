386e4fa2912681b59388af4a112eb0936ba4649c
ner <tjwor10@naver.com>
2023-04-24 11:13:06 +0900

    let const

---
+++ let const.js
@@ -0,0 +1,25 @@
+let x = 1;
+let y = x;
+console.log('변경 전',x ,y);
+
+x = 'hello world';
+console.log('변경 후',x,y);
+
+let m = 1;
+console.log('첫 선언은 가능하지만' , m);
+
+m = 2;
+console.log('두 번째 선언 할땐 다르게 선언 해줘야함' , m);
+
+const PI = 3.14;
+console.log('(const(상수):바뀌지 않는 값), 원주율', PI);
+
+//let a = 1;
+//let b = 2'
+// ...
+
+let a = 1, b = 2, c =3;
+const X = 4, Y = 5, Z = 6;
+
+console.log(a,b,c);
+console.log(X,Y,Z);
