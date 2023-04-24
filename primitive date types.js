16b8e4d1dd26abc8ca84fa2c57e27c9bcb92b126
ner <tjwor10@naver.com>
2023-04-24 13:31:38 +0900

    types

---
+++ primitive date types.js
@@ -0,0 +1,23 @@
+const a = true , b = 123.45, c = '안녕하세요!';
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(c, typeof c);
+
+let isEmployed = true;
+let isMarried = false;
+
+console.log('직업 있음:', isEmployed);
+console.log('기혼:', isMarried);
+
+const A = 1 > 2;
+const B = 1 < 2;
+
+console.log(A, typeof A);
+console.log(B, typeof B);
+
+let integer = 100;
+let real = 12.34;
+let negative = -99;
+
+console.log(integer, real, negative);
