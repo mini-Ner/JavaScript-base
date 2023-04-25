784e7556d60a2ae3cac1d663ca3aa296130fd421
ner <tjwor10@naver.com>
2023-04-25 09:23:57 +0900

    string2

--- string.js
+++ string.js
@@ -9,3 +9,13 @@ let word3 = `안녕하세요 반갑습니다~! \n
 저는 \\ 홍길동 \\ 입니다~`;
 
 console.log(word3);
+
+const NAME = '홍길동';
+let age = 20;
+let married = false;
+
+console.log(
+  `제 이름은 ${NAME}, 나이는 ${age}세구요,\
+  ${married ? '기혼' : '미혼'}입니다.`
+);
+console.log(`일 더하기 이는 ${1 + 2}이다`);
