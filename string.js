4d3e2171fb207897fe126a105ebdcb731e5a7bb5
ner <tjwor10@naver.com>
2023-04-25 09:17:58 +0900

    string

---
+++ string.js
@@ -0,0 +1,11 @@
+let word1 = '작은따옴표 안에\'작은따옴표\' 사용';
+let word2 = "큰따옴표 안에\"큰따옴표\" 사용";
+
+console.log(word1, word2);
+
+console.log('안녕하세요\n반갑습니다');
+
+let word3 = `안녕하세요 반갑습니다~! \n
+저는 \\ 홍길동 \\ 입니다~`;
+
+console.log(word3);
