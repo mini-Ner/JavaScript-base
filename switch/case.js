ceee550a81b995963d34ebb9c54c88702c675d98
ner <tjwor10@naver.com>
2023-05-08 19:35:24 +0900

    case

---
+++ switch/case.js
@@ -0,0 +1,24 @@
+let month = 8;
+ let mon = "";
+switch(month) {
+  case 1 :
+  console.log("나머지 1입니다.")
+  break;
+  
+  case 2 :
+  console.log("나머지 2입니다.");
+  break;
+  
+  case 3 :
+  console.log("나머지 3입니다.");
+  break;
+
+  case 4 :
+  console.log("나머지 4입니다.");
+  break;
+
+  default: 
+  console.log("나머지  없습니다.");
+  break;
+}
+console.log(mon);
