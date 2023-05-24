8734d8d1c287371161e1f08bb69b736362d8e96a
ner <tjwor10@naver.com>
2023-05-24 14:10:17 +0900

    concat

---
+++ concat.js
@@ -0,0 +1,13 @@
+var str1 = 'java';
+var str2 = 'script';
+
+var str3 = str1.concat(str2);
+
+console.log(str3);
+
+function add(a,b){
+  return a + b;
+}
+
+const sum = add(3, 4);
+console.log(sum);
