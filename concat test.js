86fea25b4f5a43e98444c0981b9fca6eb2e00a18
ner <tjwor10@naver.com>
2023-05-24 17:08:47 +0900

    concat test

---
+++ concat test.js
@@ -0,0 +1,20 @@
+function add(num1,num2) {
+  return num1 + num2;
+}
+
+//function overadd(operator) {
+//  const result = operator(3, 2);
+//  console.log(result);
+//}
+//overadd(add);
+//function cocomong(coco) {
+//  var result = coco(1,2);
+//  console.log(result);
+//}
+//cocomong(add);
+
+function print(name, age) {
+  console.log(`${name} ${age}`);
+}
+
+print(3,30);
